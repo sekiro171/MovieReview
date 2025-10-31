fb1fe140197987ce1cab412dfa2b4b0e3c7e5820
Lab cua Linh <kanekitv171@gmail.com>
2025-10-31 10:34:54 +0700

    Upload delete movie

--- Group3/src/main/java/com/example/Group3/confict/controller/MovieController.java
+++ Group3/src/main/java/com/example/Group3/confict/controller/MovieController.java
@@ -3,8 +3,11 @@ package com.example.Group3.confict.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +56,17 @@ public class MovieController {
 
         movieService.updateMovie(id, title, genre, director, releaseYear, synopsis, coverImageUrl);
         return "redirect:/movie";
+    }
+
+    @DeleteMapping("/movie/delete/{id}")
+    public ResponseEntity<String> deleteMovie(@PathVariable int id, Model model) {
+        if (movieService.deleteMovie(id)) {
+            String successMessage = "Movie deleted successfully.";
+            return ResponseEntity.ok(successMessage);
+        } else {
+            String errorMessage = "Movie deletion failed. Movie not found.";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        }
     }
 
 }
