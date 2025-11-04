8fb0b6a676cf4d7636d5307438a4e28ac67848e3
phanhoaian1203 <phanhoaian10b3@gmail.com>
2025-11-04 09:54:12 +0700

    add_search

--- Group3/src/main/java/com/example/Group3/confict/repository/MovieRepository.java
+++ Group3/src/main/java/com/example/Group3/confict/repository/MovieRepository.java
@@ -1,5 +1,7 @@
 package com.example.Group3.confict.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.Group3.confict.model.Movie;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
-
+    List<Movie> findByTitleContainingIgnoreCase(String keyword);
 }
